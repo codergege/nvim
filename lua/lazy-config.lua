4abb58130aa68a128d3b83b6b238037ddec93e4c
codergege <codergege@163.com>
2025-01-06 23:11:39 +0800

    Modify lazy install config
    On branch master
    Changes to be committed:
    modified:   lua/lazy-config.lua

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -7,16 +7,17 @@ function mod.available()
 end
 
 function mod.install()
-  vim.notify("Installing plugin manager lazy.nvim ...")
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-  vim.notify("Plugin manager installed complete")
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 
 function mod.setup()
